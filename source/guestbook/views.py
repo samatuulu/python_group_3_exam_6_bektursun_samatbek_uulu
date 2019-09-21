3c1470b68618258b2deecdd1e021e19a5f17111d
Bektursun <samatuulu11@gmail.com>
2019-09-21 18:56:54 +0600

    Now I added function for delete. Each posts are available to delte. Worked on design little bit, for that created file  Finished 5th step.

--- source/guestbook/views.py
+++ source/guestbook/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from guestbook.forms import BookForm
 from guestbook.models import GuestBook, BOOK_ACTIVE_CHOICE
@@ -27,3 +27,32 @@ def book_create(request, *args, **kwargs):
             return response
         else:
             return render(request, 'book_create.html', context={'form': form})
+
+
+def book_update(request, pk, *args, **kwargs):
+    book = get_object_or_404(GuestBook, pk=pk)
+    if request.method == 'GET':
+        form = BookForm(data={
+            'name': book.name,
+            'email': book.email,
+            'text': book.text
+        })
+        return render(request, 'book_update.html', context={'book': book, 'form': form})
+    elif request.method == 'POST':
+        form = BookForm(data=request.POST)
+        if form.is_valid():
+            book.name = form.cleaned_data['name']
+            book.email = form.cleaned_data['email']
+            book.text = form.cleaned_data['text']
+            book.save()
+
+            return redirect('home')
+
+
+def book_delete(request, pk):
+    book = get_object_or_404(GuestBook, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'book_delete.html', context={'book': book})
+    elif request.method == 'POST':
+        book.delete()
+        return redirect('home')
--- source/main/urls.py
+++ source/main/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from guestbook.views import home, book_create
+from guestbook.views import home, book_create, book_update, book_delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('book/add/', book_create, name='book_create'),
+    path('book/<int:pk>/update/', book_update, name='book_update'),
+    path('book/<int:pk>/delete/', book_delete, name='book_delete'),
 ]
